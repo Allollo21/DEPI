3e0d40338fa349f7fc8c51ed7bb3944158a7ea6c
Ali Reda Abdulbaky <113148517+Allollo21@users.noreply.github.com>
2025-09-18 14:22:01 +0300

    ADDED MVC Day01 Assignment

---
+++ MVC_Day01/Assignment/Assignment/Controllers/SudentController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment.Controllers
+{
+    public class StudentController : Controller
+    {
+        // URL: /Student/Welcome
+        public ContentResult Welcome()
+        {
+            ContentResult result = new ContentResult();
+            result.Content = "Welcome to Student Management System!";
+            return result;
+        }
+
+        // URL: /Student/ShowInfo
+        public ViewResult ShowInfo()
+        {
+            ViewResult result = new ViewResult();
+            result.ViewName = "StudentInfo";
+            return result;
+        }
+
+        // URL: /Student/CheckGrade?grade=85
+        public IActionResult CheckGrade(int grade)
+        {
+            if (grade >= 60)
+            {
+                return View("Success");
+            }
+            else
+            {
+                return Content($"Sorry, grade {grade} is failing. Study harder!");
+            }
+        }
+
+        // Action 4: Display list of students
+        // URL: /Student/List
+        public IActionResult List()
+        {
+            return View("StudentList");
+        }
+    }
+}
---
+++ MVC_Day01/Assignment/Assignment/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment.Models
+{
+    namespace SimpleDemo.Models
+    {
+        public class Student
+        {
+            public int Id { get; set; }
+            public string? Name { get; set; }
+            public int Age { get; set; }
+            public string? Email { get; set; }
+            public double GPA { get; set; }
+            public string? Major { get; set; }
+        }
+    }
+}
