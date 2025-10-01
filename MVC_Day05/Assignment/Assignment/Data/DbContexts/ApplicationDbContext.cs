c3e47d6b4183758418672a72eaefeff1e607788d
Ali Reda Abdulbaky <113148517+Allollo21@users.noreply.github.com>
2025-10-01 03:18:19 +0300

    ADDED MVC Day05 Assignment

---
+++ MVC_Day05/Assignment/Assignment/Data/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Assignment.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assignment.Data.DbContexts
+{
+    public class ApplicationDbContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseLazyLoadingProxies().UseSqlServer("Server=.;Database=Corporation;Trusted_Connection=True;TrustServerCertificate=True;");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            DatabaseSeeder.SeedData(modelBuilder);
+        }   
+
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<StudentCourseResult> StudentCourseResults { get; set; }
+
+    }
+}
