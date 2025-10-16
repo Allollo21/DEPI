e5f86a9759e8c3780a4d87a3f44db8b2159bb7c3
Ali Reda Abdulbaky <113148517+Allollo21@users.noreply.github.com>
2025-10-16 05:00:57 +0300

    ADDED Day08 MVC Assignment

---
+++ MVC_Day08/TaskManagementSystem/Service/Services/TaskService.cs
@@ -0,0 +1,54 @@
+﻿using Application.Interfaces;
+using Domain.Interfaces;
+using Domain.Models;
+
+namespace Application.Services
+{
+    public class TaskService : ITaskService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public TaskService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IEnumerable<TaskItem>> GetAllTasksAsync()
+        {
+            return await _unitOfWork.Tasks.GetAllAsync();
+        }
+
+        public async Task<IEnumerable<TaskItem>> GetFilteredTasksByCompletionAsync(bool? isCompleted)
+        {
+            return await _unitOfWork.Tasks.GetFilteredByCompletionAsync(isCompleted);
+        }
+
+        public async Task<TaskItem?> GetTaskByIdAsync(int id)
+        {
+            return await _unitOfWork.Tasks.GetByIdAsync(id);
+        }
+
+        public async Task<TaskItem> CreateTaskAsync(TaskItem task)
+        {
+            var added = await _unitOfWork.Tasks.AddAsync(task);
+            await _unitOfWork.SaveAsync();
+            return added;
+        }
+
+        public async Task UpdateTaskAsync(TaskItem task)
+        {
+            await _unitOfWork.Tasks.UpdateAsync(task);
+            await _unitOfWork.SaveAsync();
+        }
+
+        public async Task DeleteTaskAsync(int id)
+        {
+            var task = await _unitOfWork.Tasks.GetByIdAsync(id);
+            if (task != null)
+            {
+                await _unitOfWork.Tasks.DeleteAsync(task);
+                await _unitOfWork.SaveAsync();
+            }
+        }
+    }
+}
